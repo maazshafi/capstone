def1cafc4dd24de79696d31cedabd24ee7c91d2a
Maaz Shafi <maaz_shafi@hotmail.com>
2021-08-30 18:42:33 -0400

    Setup on models and cors with test route

--- app.py
+++ app.py
@@ -3,10 +3,27 @@ from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
+from models import setup_db
+
 def create_app(test_config=None):
   # create and configure the app
   app = Flask(__name__)
-  CORS(app)
+  setup_db(app)
+  CORS(app, resources={'/': {'origins': '*'}})
+
+  # CORS Headers
+  @app.after_request
+  def after_request(response):
+    response.headers.add('Access-Control-Allow-Headers',
+                      'Content-Type,Authorization,true')
+    response.headers.add('Access-Control-Allow-Methods',
+                          'GET,PUT,POST,DELETE,OPTIONS')
+    return response
+
+  @app.route('/movies')
+  def get_movies():
+    # query = 
+    return
 
   return app
 
--- models.py
+++ models.py
@@ -1,1 +1,95 @@
+import os
+from posix import PRIO_USER
+from flask.json import tojson_filter
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.sql.sqltypes import DateTime
+from flask_sqlalchemy import SQLAlchemy
+import json
+from flask_migrate import Migrate
+
+USERNAME = os.environ.get('capstone_db_username')
+PASSWORD = os.environ.get('capstone_db_password')
+
+database_name = "capstone_test"
+database_path = "postgres://{}:{}@{}/{}".format(USERNAME, PASSWORD, 'localhost:5432', database_name)
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+'''
+setup_db(app)
+    binds a flask application and a SQLAlchemy service
+'''
+
+def setup_db(app, database_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    migrate.init_app(app)
+    db.create_all()
+
+class Movie(db.Model):
+    __tablename__ = 'movies'
+
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    release_date = Column(DateTime)
+
+    def __init__(self, title, release_date):
+        self.title = title
+        self.release_date = release_date
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'release_date': self.release_date
+        }
+
+class Actor(db.Model):
+    __tablename__ = 'actors'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    age = Column(Integer)
+    gender = Column(String)
+
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'age': self.age,
+            'gender': self.gender
+        }
+
+    
 
